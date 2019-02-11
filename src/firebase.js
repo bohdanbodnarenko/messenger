f0a918881c4181bc87078441dac1387fc57e22c5
bodyabodnarenko <bodya.bodnarenko@gmail.com>
2019-02-11 18:34:07 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+const config = {
+    apiKey: "AIzaSyC_GxBp7Jl2BTjIGdPKgnOpIPmsX22XfuY",
+    authDomain: "slack-messanger.firebaseapp.com",
+    databaseURL: "https://slack-messanger.firebaseio.com",
+    projectId: "slack-messanger",
+    storageBucket: "",
+    messagingSenderId: "1006195291841"
+};
+firebase.initializeApp(config);
+
+export default firebase;
